f591f3ec6ed4d38f514a879e623b120e6864851b
KLINTON J <klintonece@gmail.com>
2024-10-24 15:01:51 +0530

    dev run

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "genesyscloud_architect_user_prompt" "TestTerraformPrompt" {
   description = "Prompt"
   name        = "Demo_Prompt"
   resources {
-    tts_string = "This is demo Prompt for testing purpose"
+    tts_string = "This is demo Prompt for testing purposes"
     language   = "en-us"
     text       = "This is demo Prompt for dev purpose"
   }
