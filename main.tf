ca3682eaf222c71a4e21f30261fe2682d0198d6f
KLINTON J <klintonece@gmail.com>
2024-10-25 10:46:51 +0530

    test

--- main.tf
+++ main.tf
@@ -9,7 +9,11 @@ terraform {
     }
   }
 
-  
+   backend "s3" {
+    bucket = "test-terraform-bucket-330"
+    key    = "key/terraform.tfstate"
+    region = "ap-south-1"
+  }
 }
 
 resource "genesyscloud_architect_user_prompt" "TestTerraformPrompt" {
